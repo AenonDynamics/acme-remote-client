40ccc17a2511a92a0fdee4bc0a9888bbfd2bc60e
Andi Dittrich <andi.dittrich@a3non.org>
2016-04-24 18:59:53 +0200

    Replaced stdout cert output by --out option

--- acme_tiny.py
+++ acme_tiny.py
@@ -188,11 +188,15 @@ def main(argv):
     parser.add_argument("--acme-dir", required=True, help="path to the .well-known/acme-challenge/ directory")
     parser.add_argument("--quiet", action="store_const", const=logging.ERROR, help="suppress output except for errors")
     parser.add_argument("--ca", default=DEFAULT_CA, help="certificate authority, default is Let's Encrypt")
-
+    parser.add_argument("--out", required=True, help="the output filename")
+    
     args = parser.parse_args(argv)
     LOGGER.setLevel(args.quiet or LOGGER.level)
     signed_crt = get_crt(args.account_key, args.csr, args.acme_dir, log=LOGGER, CA=args.ca)
-    sys.stdout.write(signed_crt)
+    
+    # write output file
+    with open(args.out, "w") as cert_file:
+        cert_file.write(signed_crt)
 
 if __name__ == "__main__": # pragma: no cover
     main(sys.argv[1:])
